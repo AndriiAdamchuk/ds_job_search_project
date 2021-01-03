d5639128f3123f32feaf8ecb24839bbdde629909
Andrii Adamchuk <andrii.adamchuk@gmail.com>
2021-01-03 19:45:14 +0100

    Data collection scripts

---
+++ collect_data.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  3 18:09:59 2021
+
+@author: andrii
+"""
+
+import selenium_scrapping_script as sss
+import pandas as pd 
+path = "/Users/andrii/Documents/Git/ds_job_search_project/chromedriver"
+
+df = sss.get_jobs('data analyst', 10, False, path, 10)
+df
