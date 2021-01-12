ff9888f5332729f4358f3b5fc628d111cc47a333
Andrii Adamchuk <andrii.adamchuk@gmail.com>
2021-01-12 16:55:21 +0100

    Update build_model.py

--- build_model.py
+++ build_model.py
@@ -82,3 +82,14 @@ tpred_rf = gs.best_estimator_.predict(X_test)
 from sklearn.metrics import mean_absolute_error
 mean_absolute_error(y_test,tpred_lm)
 mean_absolute_error(y_test,tpred_rf)
+
+import pickle
+pickl = {'model': gs.best_estimator_}
+pickle.dump( pickl, open( 'model_file' + ".p", "wb" ) )
+
+file_name = "model_file.p"
+with open(file_name, 'rb') as pickled:
+    data = pickle.load(pickled)
+    model = data['model']
+
+#model.predict(X_test.iloc[1,:].values.reshape(1,-1))
